010dea950a03982e5cd8ec299a71be870ae04d54
Madison Pooser <m.pooser317@gmail.com>
2021-07-24 15:16:13 -0400

    Saving MechaCarChallenge file

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -12,3 +12,5 @@ summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_cleara
 suspension_coil <- read.csv(file = 'Suspension_Coil.csv', check.names = F,stringsAsFactors = F)
 total_summary <- suspension_coil %>% summarize(Mean=mean(PSI),Median=median(PSI),Variance=var(PSI),SD=sd(PSI))
 lot_summary <- suspension_coil %>% group_by(Manufacturing_Lot) %>% summarize(Mean=mean(PSI),Median=median(PSI),Variance=var(PSI),SD=sd(PSI), .groups='keep')
+?t.test
+t.test(mechacar_mpg,mu=1500)
